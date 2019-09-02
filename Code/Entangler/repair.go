250b9d1826bccbdc3b0ee056ae01e8513f0c4ce5
Racin Nygaard <racin.nygaard@gmail.com>
2019-09-02 13:07:32 +0200

    repair

--- Code/Entangler/repair.go
+++ Code/Entangler/repair.go
@@ -22,6 +22,16 @@ func (l *Lattice) Reconstruct() ([]byte, error) {
 	return out, nil
 }
 
+func getMaxStrandMatch(arr []int) (index, max int) {
+	for i := 0; i < len(arr); i++ {
+		if arr[i] > max {
+			max = arr[i]
+			index = i
+		}
+	}
+	return
+}
+
 func (l *Lattice) HierarchicalRepair(block *Block) *Block {
 	if block == nil {
 		return nil
@@ -35,15 +45,31 @@ func (l *Lattice) HierarchicalRepair(block *Block) *Block {
 		if data == nil {
 			return block
 		}
-		for i := 0; i < len(data.Left); i++ {
-			if data.Left[i] == nil {
-				continue
+		strandMatch := make([]int, Alpha)
+		for i := 0; i < Alpha; i++ {
+			if data.Left[i] != nil {
+				strandMatch[i] = strandMatch[i] + 1
 			}
 			if data.Right[i] == nil {
-				continue
+				strandMatch[i] = strandMatch[i] + 1
 			}
 		}
 
+		mI, mC := getMaxStrandMatch(strandMatch)
+
+		// Result, _ := Entangler.XORByteSlice(contentA, contentB)
+
+		// If its 2 we already have the parities we need.
+		if mC == 2 {
+			// XOR Left & Right
+		} else if mC == 1 {
+			// Missing left or right. Repair one of them.
+			b := l.HierarchicalRepair(&Block{Base: data.Left, Data: nil})
+			// XOR recovered with already existing
+		} else {
+			bLeft := l.HierarchicalRepair(&Block{Base: data.Left, Data: nil})
+			bRight := l.HierarchicalRepair(&Block{Base: data.Right, Data: nil})
+		}
 		return &Block{Base: data, Data: nil}
 
 	}
@@ -57,6 +83,69 @@ func (l *Lattice) HierarchicalRepair(block *Block) *Block {
 	return block
 }
 
+func (l *Lattice) XORBlocks(left *Block, right *Block) (*Block, error) {
+	// Case 1: Both are Parity
+	// Case 2: One is Parity, one is Data
+	var retBlock *Block
+	var err error
+	var ok bool
+	var data *Data
+	var p1, p2 *Parity
+
+	data, ok = left.Base.(*Data)
+	if !ok {
+		p1, ok = left.Base.(*Parity)
+		if !ok {
+			return nil, errors.New("cannot type cast left block")
+		}
+	} else {
+		data, ok = right.Base.(*Data)
+		if !ok {
+			p2, ok = right.Base.(*Parity)
+			if !ok {
+				return nil, errors.New("cannot type cast right block")
+			}
+		}
+	}
+
+	if p1 != nil && data != nil {
+		// Two parities
+		if p1.Right != p2.Left {
+			return nil, errors.New("parity blocks do not match")
+		}
+
+		p1.Right.Data, err = XORByteSlice(p1.Data, p2.Data)
+		if err != nil {
+			return nil, err
+		}
+
+	} else if p2 != nil && data != nil {
+		// Two parities
+		if p1.Right != p2.Left {
+			return nil, errors.New("parity blocks do not match")
+		}
+
+		p1.Right.Data, err = XORByteSlice(p1.Data, p2.Data)
+		if err != nil {
+			return nil, err
+		}
+
+	} else {
+		// Two parities
+		if p1.Right != p2.Left {
+			return nil, errors.New("parity blocks do not match")
+		}
+
+		p1.Right.Data, err = XORByteSlice(p1.Data, p2.Data)
+		if err != nil {
+			return nil, err
+		}
+
+		return &Block{Base: p1.Right}, nil
+	}
+
+	return
+}
 func (l *Lattice) RoundrobinRepair(block *data.LatticeBlock) {
 
 }
